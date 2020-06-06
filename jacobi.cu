215bb63372502adf698b7427960c307566840d46
Fran González <fran98@correo.ugr.es>
2020-06-06 12:11:04 +0200

    implementada primera versión clase jacobi usando matrices csr.

--- jacobi.cu
+++ jacobi.cu
@@ -3,32 +3,28 @@
 //
 
 #include "jacobi.cuh"
-#include <reduce.cuh>
 
-
-
-float jacobi::norma(const float *r) {
-    float sum_cuadrados = 0;
+double jacobi::norma(const double *r) {
+    double sum_cuadrados = 0;
     for (size_t i = 0; i < getFilas(); i++) {
         sum_cuadrados += pow(r[i], 2);
     }
     return sqrt(sum_cuadrados);
 }
 
-float *jacobi::calculaDiagonal() {
-    float *D = new float[matriz.filas];
-    int fila = 0;
+double *jacobi::calculaDiagonal() {
+    double *D = new double[matriz.filas];
+    int pos = 0;
+    int lim = min(getFilas(), getColumnas());
     int i = 0;
-    while (fila < getFilas()) {
-        if (matriz.row_ptr[fila] == matriz.row_ptr[fila + 1]) {
-            D[fila] = 0;
-            fila++;
-        } else if (matriz.col_ind[i] == fila) {
-            D[fila] = matriz.val[i];
-            fila++;
-        } else if (i >= matriz.row_ptr[fila + 1]) {
-            D[fila] = 0;
-            fila++;
+    while (pos < lim) {
+        if (matriz.row_ptr[pos] == matriz.row_ptr[pos + 1] || i >= matriz.row_ptr[pos + 1]) {
+            D[pos] = 0;
+            pos++;
+        } else if (matriz.col_ind[i] == pos) {
+            D[pos] = matriz.val[i];
+            while (i < matriz.row_ptr[pos + 1]) i++;
+            pos++;
         } else i++;
     }
     return D;
@@ -38,7 +34,7 @@ int jacobi::getFilas() {
     return matriz.filas;
 }
 
-float *jacobi::getDiagonal() const {
+double *jacobi::getDiagonal() const {
     return diagonal;
 }
 
@@ -51,11 +47,11 @@ void jacobi::inversaDiagonal() {
     }
 }
 
-float *jacobi::getInversa() const {
+double *jacobi::getInversa() const {
     return inversa;
 }
 
-void jacobi::diferencia(float *b, float *q, float *r, int n) {
+void jacobi::diferencia(double *b, double *q, double *r, int n) {
     for (size_t i = 0; i < n; i++) {
         r[i] = b[i] - q[i];
     }
@@ -63,4 +59,31 @@ void jacobi::diferencia(float *b, float *q, float *r, int n) {
 
 int jacobi::getColumnas() {
     return matriz.columnas;
+}
+
+double *jacobi::multiplicacionMV() {
+    dim3 block_size(BLOCK_SIZE);
+    dim3 grid_size{};
+    const unsigned int warp_size = 32; /// One warp per row
+    grid_size.x = (warp_size * getFilas() + block_size.x - 1) / block_size.x;
+
+    cudaMemcpy(x_d, x_h.data(), getColumnas() * sizeof(double), cudaMemcpyHostToDevice);
+
+    csr_spmv_vector_kernel<double><<<grid_size, block_size>>>(getFilas(), col_ind, row_ptr, A, x_d, y_d);
+
+    cudaMemcpy(y_h, y_d, getFilas() * sizeof(double), cudaMemcpyDeviceToHost);
+
+    return y_h;
+}
+
+double *jacobi::getYH() {
+    return y_h;
+}
+
+double &jacobi::getXH(int i) {
+    return x_h[i];
+}
+
+const vector<double> &jacobi::getXH() const {
+    return x_h;
 }
--- jacobi.cuh
+++ jacobi.cuh
@@ -5,6 +5,7 @@
 #define JACOBI_H
 
 #include <csr_formatter.h>
+#include <reduce.cuh>
 #include <cmath>
 
 using namespace std;
@@ -18,47 +19,123 @@ __global__ void csr_spmv_vector_kernel(
         const data_type *x,
         data_type *y);
 
-class jacobi {
-public:
-    jacobi(const CSR &m) {
-        if (m.filas != 0)
-            this->matriz = m;
-        else
-            throw std::invalid_argument("la matriz no está inicializada");
 
-        diagonal = calculaDiagonal();
-        inversa = new float[getFilas()];
-        inversaDiagonal();
+template<typename data_type>
+__global__ void csr_spmv_vector_kernel(
+        unsigned int n_rows,
+        const unsigned int *col_ids,
+        const unsigned int *row_ptr,
+        const data_type *data,
+        const data_type *x,
+        data_type *y) {
+    const unsigned int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int warp_id = thread_id / 32;
+    const unsigned int lane = thread_id % 32;
+
+//    DEBUG //////////////
+//    if(thread_id == 0){
+//        printf("Vector datos: ");
+//        for (int i = 0; i < row_ptr[n_rows]; i++) printf("%f ", data[i]);
+//        printf("\n");
+//        printf("Vector filas: ");
+//        for (int i = 0; i < n_rows+1; i++) printf("%d ", row_ptr[i]);
+//        printf("\n");
+//        printf("Vector columnas: ");
+//        for (int i = 0; i < row_ptr[n_rows]; i++) printf("%d ", col_ids[i]);
+//        printf("\n");
+//    }
+
+    const unsigned int row = warp_id; ///< un warp por fila
+
+    data_type sum = 0;
+    if (row < n_rows) {
+        const unsigned int row_start = row_ptr[row];
+        const unsigned int row_end = row_ptr[row + 1];
+
+        for (unsigned int element = row_start + lane; element < row_end; element += 32)
+            sum += data[element] * x[col_ids[element]];
     }
 
-    float *getInversa() const;
+    sum = warp_reduce(sum);
 
-    float *getDiagonal() const;;
+    if (lane == 0 && row < n_rows) {
+        y[row] = sum;
+    }
+}
 
-    void multiplicacionMV();
+
+class jacobi {
+public:
+    jacobi(const CSR &m, const int &block_size_arg) : x_h(m.filas,1), BLOCK_SIZE(block_size_arg){
+        if (m.filas == 0)
+            throw std::invalid_argument("la matriz no está inicializada");
+
+        matriz = m;
+        y_h = new double[getFilas()];
+        diagonal = calculaDiagonal();
+        inversa = new double[getFilas()];
+        inversaDiagonal();
+        cudaMalloc(&A, sizeof(double) * matriz.val.size());
+        cudaMalloc(&col_ind, sizeof(double) * matriz.col_ind.size());
+        cudaMalloc(&row_ptr, sizeof(double) * matriz.row_ptr.size());
+        cudaMalloc(&x_d, sizeof(double) * getColumnas());
+        cudaMalloc(&y_d, sizeof(double) * getFilas());
+
+        cudaMemcpy(A, matriz.val.data(), matriz.val.size() * sizeof(double), cudaMemcpyHostToDevice);
+        cudaMemcpy(col_ind, matriz.col_ind.data(), matriz.col_ind.size() * sizeof(double), cudaMemcpyHostToDevice);
+        cudaMemcpy(row_ptr, matriz.row_ptr.data(), matriz.row_ptr.size() * sizeof(double), cudaMemcpyHostToDevice);
+    }
+
+
+    ~jacobi() {
+        free(y_h);
+        free(diagonal);
+        free(inversa);
+        cudaFree(A);
+        cudaFree(x_d);
+        cudaFree(y_d);
+        cudaFree(col_ind);
+        cudaFree(row_ptr);
+    }
+
+    double *getInversa() const;
+
+    double *getDiagonal() const;
+
+    double *multiplicacionMV();
 
     void inversaDiagonal();
 
-    float *calculaDiagonal();
+    double *calculaDiagonal();
 
-    float norma(const float *r);
+    double norma(const double *r);
 
-    void diferencia(float *b, float *q, float *r, int n);
-
+    void diferencia(double *b, double *q, double *r, int n);
 
     int getFilas();
 
     int getColumnas();
 
-    const CSR &getMatriz() const {
-        return matriz;
-    }
+    const CSR &getMatriz() const { return matriz; }
 
     CSR matriz;
-private:
-    float *diagonal;
-    float *inversa;
 
+    double *getYH();
+
+    double &getXH(int i);
+
+    const vector<double> &getXH() const;
+
+private:
+    double *diagonal;
+    double *inversa;
+    double *y_h;
+    vector<double> x_h;
+
+    double *A, *x_d, *y_d;
+    unsigned int *col_ind, *row_ptr;
+
+    const int BLOCK_SIZE;
 };
 
 #endif //JACOBI_H
